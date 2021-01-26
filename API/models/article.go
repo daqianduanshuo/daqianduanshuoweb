b9ca39f297f39b07856bfc84cbd17bf4983235e0
shch <shch1289@gmail.com>
2021-01-26 23:31:03 +0800

    feat: API 文章列表返回数据添加create_at title 字段

--- API/models/article.go
+++ API/models/article.go
@@ -1,16 +1,27 @@
 package models
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// OwnModel Model
+type OwnModel struct {
+	ID        uint      `gorm:"primary_key" json:"-"`
+	CreatedAt time.Time `json:"create_at"`
+	UpdatedAt time.Time `json:"-"`
+	DeletedAt time.Time `json:"-"`
+}
+
 // Article Model
 type Article struct {
-	gorm.Model `json:"-"`
-	Content    string `gorm:"text" json:"content" binding:"required"`
-	Videos     string `gorm:"varchar(2048)" json:"videos" binding:"required"`
-	Tags       []Tag  `json:"tags" binding:"required"`
+	OwnModel
+	Title   string `gorm:"varchar(256)" json:"title" binding:"required"`
+	Content string `gorm:"text" json:"content" binding:"required"`
+	Videos  string `gorm:"varchar(2048)" json:"videos" binding:"required"`
+	Tags    []Tag  `json:"tags" binding:"required"`
 }
 
 // Tag Model
